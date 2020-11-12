002d012d59e24cc01ac7354de72672a7829e9e44
Petrkolo9 <71827140+Petrkolo9@users.noreply.github.com>
2020-11-12 17:13:05 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	cout << "C- - - -D" << endl << "| | | | | \\" << endl << " - - - - - " << endl << "| | | | | | \\" << endl << " - - - - - - " << endl << "| | | | | | | \\" << endl << " - - - - - - - " << endl << "| | | | | | | | \\" << endl << "A- - - - - - - -B" << endl;
+	
+	
+	
+	return 0;
+}
