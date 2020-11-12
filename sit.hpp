4c6e00ba90097b921e18a8f4992d8ee797b3aa1a
Petrkolo9 <71827140+Petrkolo9@users.noreply.github.com>
2020-11-12 18:53:13 +0100

    Update sit.hpp

--- sit.hpp
+++ sit.hpp
@@ -3,3 +3,30 @@
 #include <vector>
 
 using namespace std;
+
+class sit{
+	public:
+		double h;				//krok site
+		double Ax;
+		double Ay;
+		double Bx;
+		double By;
+		double Cx;
+		double Cy;
+		double Dx;
+		double Dy;
+		
+		sit(double _Ax, double _Ay, double _Bx, double _By, double _Cx, double _Cy, double _Dx, double _Dy, int _h);
+};
+
+sit::sit(double _Ax, double _Ay, double _Bx, double _By, double _Cx, double _Cy, double _Dx, double _Dy, int _h){
+	Ax = _Ax;
+	Ay = _Ax;
+	Bx = _Ax;
+	By = _Ax;
+	Cx = _Ax;
+	Cy = _Ax;
+	Dx = _Ax;
+	Dy = _Ax;
+	h = _h;
+}
