5ef01598f351a178d804a19214b45e6d528110f7
Petrkolo9 <71827140+Petrkolo9@users.noreply.github.com>
2020-12-04 03:43:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
 
 #include "sit.hpp"
+#include "uzel.hpp"
+
 
 using namespace std;
 
@@ -10,28 +11,18 @@ int main() {
 	//Nadpis
 	cout << "+++----------------------------------------------+++" << endl << "+++----------------------------------------------+++" << endl << "||| RESENI POISSONOVY ROVNICE METODOU SITI VE 2D |||" << endl << "+++----------------------------------------------+++"<< endl << "+++----------------------------------------------+++" << endl << endl;
 	
-	//Schema site
-	cout << "C- - - -D" << endl << "| | | | | \\" << endl << " - - - - - " << endl << "| | | | | | \\" << endl << " - - - - - - " << endl << "| | | | | | | \\" << endl << " - - - - - - - " << endl << "| | | | | | | | \\" << endl << "A- - - - - - - -B" << endl << endl;
-	
-	//Zadani krajnich bodu a kroku site
-	vector<double> A(2);
-	vector<double> B(2);
-	vector<double> C(2);
-	vector<double> D(2);
-	double h;
-	cout << "Zadani souradnic [x,y] krajnich bodu (bez mezery):" << endl;
-	cout << "A = ";
-	cin >> A[0] >> A[1];
-	cout << "B = ";
-	cin >> B[0] >> B[1];
-	cout << "C = ";
-	cin >> C[0] >> C[1];
-	cout << "D = ";
-	cin >> D[0] >> D[1];
-	cout << endl << "Zadani kroku site:" << endl;
-	cout << "h = ";
-	cin >> h;
 
+	sit g(1, 2, 10, 5, 10, 5);
+
+	for(int i = 0; i <= g.n2; i++){
+		cout << g.y[i] << "\t" ;
+	}
 	
+	cout << endl;
+	
+	for(int i = 0; i <= g.n1; i++){
+		cout << g.x[i] << "\t" ;
+	}
+
 	return 0;
 }
