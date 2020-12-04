60ed3a1ded9a405da8a5be2659e9c977ea4b832f
Petrkolo9 <71827140+Petrkolo9@users.noreply.github.com>
2020-12-04 03:44:59 +0100

    Update sit.hpp

--- sit.hpp
+++ sit.hpp
@@ -6,27 +6,39 @@ using namespace std;
 
 class sit{
 	public:
-		double h;				//krok site
+		vector<double> x;
+		vector<double> y;
 		double Ax;
 		double Ay;
-		double Bx;
-		double By;
-		double Cx;
-		double Cy;
-		double Dx;
-		double Dy;
-		
-		sit(double _Ax, double _Ay, double _Bx, double _By, double _Cx, double _Cy, double _Dx, double _Dy, int _h);
+		double d1;
+		double d2;
+		double h1;
+		double h2;
+		int n1;
+		int n2;
+	
+		sit(double _Ax, double _Ay, double _d1, double _d2, int _n1, int _n2);
 };
 
-sit::sit(double _Ax, double _Ay, double _Bx, double _By, double _Cx, double _Cy, double _Dx, double _Dy, int _h){
+sit::sit(double _Ax, double _Ay, double _d1, double _d2, int _n1, int _n2){
 	Ax = _Ax;
-	Ay = _Ax;
-	Bx = _Ax;
-	By = _Ax;
-	Cx = _Ax;
-	Cy = _Ax;
-	Dx = _Ax;
-	Dy = _Ax;
-	h = _h;
+	Ay = _Ay;
+	d1 = _d1;
+	d2 = _d2;
+	n1 = _n1;
+	n2 = _n2;
+	
+	h1 = d1/n1;
+	h2 = d2/n2;
+	
+	x.resize(n1);
+	y.resize(n2);
+	
+	for(int i = 0; i <= n1; i++){
+		x[i] = Ax + i * h1;
+	}
+	
+	for(int i = 0; i <= n2; i++){
+		y[i] = Ay + i * h2;
+	}
 }
